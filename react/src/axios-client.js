e34563f25309e66ff28c84aef203318c580e5708
scotttigger <heg.group.spark@gmail.com>
2023-09-12 14:45:09 +0200

    changement axiosClient

--- react/src/axios-client.js
+++ react/src/axios-client.js
@@ -6,7 +6,7 @@ const axiosClient = axios.create({
 
 axiosClient.interceptors.request.use((config)=>{
     const token = localStorage.getItem('ACCESS_TOKEN')
-    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
+    // axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
     config.headers.Authorization = `Bearer ${token}`
     return config
 })
